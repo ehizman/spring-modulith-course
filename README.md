461dd0ae2da0120321df61c3ad7b2a339f66ebe5
Ehis Edemakhiota <edemaehiz@gmail.com>
2024-12-19 08:43:12 +0100

    added files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spring Modulith Course
+
+- to learn how to create pipelines for deployment to kubernetes
+- to learn how to work with kubernetes on docker desktop
---
+++ src/main/java/com/ehizman/spring_modulith_course/SpringModulithCourseApplication.java
@@ -0,0 +1,13 @@
+package com.ehizman.spring_modulith_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModulithCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModulithCourseApplication.class, args);
+	}
+
+}
